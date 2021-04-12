19661b12a7030211b7fa7ad6ba337dfb9ca0bfaf
Bricktech2000 <bricktech2000@gmail.com>
2021-04-11 21:08:05 -0400

    added `DatabaseSearch` component

--- website/private/structure/mainPosts.js
+++ website/private/structure/mainPosts.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import DatabaseSearch from '../api/databaseSearch';
 import TagList from '../components/tagList';
+import Loading from '../components/loading';
+import Card from '../components/card';
+import getPostInfo from '../../pages/api/getPostInfo';
 
 class MainPosts extends Component {
   state = {};
+
+  async componentDidMount() {
+    this.setState({
+      ids: await DatabaseSearch({
+        opr: 'or',
+        tags: [this.props.tag],
+        excl: this.props.tag == 'posts' ? null : 'zero',
+        max: 1000,
+      }),
+    });
+  }
+
   render() {
+    if (this.state.ids === undefined) return <Loading height="500vh" />;
     return (
       <React.Fragment>
         <h1 className="markup-h1">All Projects</h1>
         <TagList actives={[this.props.tag]} />
+        {this.state.ids.map((id, i) => (
+          <Card key={i} info={getPostInfo(id)} />
+        ))}
       </React.Fragment>
     );
   }
