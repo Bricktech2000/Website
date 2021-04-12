9ae39cf1d050b184e61db2a178dfe61467fc2d02
Bricktech2000 <bricktech2000@gmail.com>
2021-04-11 21:03:34 -0400

    modified `search` and `posts` page

--- website/pages/posts/index.js
+++ website/pages/posts/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import App from '../../components/struct/app';
-import HeaderPost from '../../components/struct/headerPost';
 import Nav from '../../components/struct/nav';
 import Aside from '../../components/struct/aside';
 import Main from '../../components/struct/main';
@@ -10,11 +9,10 @@ import Footer from '../../components/struct/footer';
 export default function Home() {
   return (
     <App>
-      <HeaderPost />
       <Nav />
-      <Aside />
+      {/*<Aside />
       <Main />
-      <Footer />
+      <Footer />*/}
     </App>
   );
 }
--- website/pages/search.js
+++ website/pages/search.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import App from '../components/struct/app';
+import Nav from '../components/struct/nav';
+import Aside from '../components/struct/aside';
+import Main from '../components/struct/main';
+import Footer from '../components/struct/footer';
 
 export default function Home() {
-  return <App />;
+  return (
+    <App>
+      <Nav />
+      {/*<Aside />
+      <Main />
+      <Footer />*/}
+    </App>
+  );
 }
