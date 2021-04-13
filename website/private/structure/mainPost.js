212500e80331489d58e138fe09c37e5a39ac7563
Bricktech2000 <bricktech2000@gmail.com>
2021-04-13 11:40:00 -0400

    added `Related Posts` section to `mainPost` component

--- website/private/structure/mainPost.js
+++ website/private/structure/mainPost.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import getPostInfo from '../../pages/api/getPostInfo';
+import databaseSearch from '../api/databaseSearch';
+import MosaicSmall from '../components/mosaicSmall';
+import Card from '../components/card';
 
 class MainPost extends Component {
   state = {};
@@ -7,14 +11,31 @@ class MainPost extends Component {
     this.setState({
       text: await (await fetch('/' + this.props.id + '/index.md')).text(),
     });
+    this.setState({
+      ids: await databaseSearch({
+        opr: 'like',
+        tags: (await getPostInfo(this.props.id)).tags,
+        excl: this.props.id,
+        max: 4,
+      }),
+    });
   }
 
   render() {
-    if (this.state.text === undefined) return '';
+    if (this.state.text === undefined || this.state.ids === undefined)
+      return '';
 
     return (
       <React.Fragment>
         <p className="">{this.state.text}</p>
+        <h1 className="markup-h1">Related Posts</h1>
+        <br />
+        <br />
+        <MosaicSmall>
+          {this.state.ids.map((id) => (
+            <Card key={id} info={getPostInfo(id)} />
+          ))}
+        </MosaicSmall>
       </React.Fragment>
     );
   }
