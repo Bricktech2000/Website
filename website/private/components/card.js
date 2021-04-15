7ea2cd72a22ef9f22b83bdfabf800f40e975da6e
Bricktech2000 <bricktech2000@gmail.com>
2021-04-15 12:01:28 -0400

    fixed cards in on tall aspect ratios

--- website/private/components/card.js
+++ website/private/components/card.js
@@ -8,10 +8,41 @@ import styles from './card.module.css';
 class Card extends Component {
   state = { info: null };
 
+  constructor(props) {
+    super(props);
+
+    this.ref = React.createRef();
+  }
+
   async componentDidMount() {
+    this.setState({ dir: this.props.dir });
     this.componentDidUpdate();
   }
   async componentDidUpdate() {
+    //css media query parent width
+    //https://stackoverflow.com/questions/12251750/can-media-queries-resize-based-on-a-div-element-instead-of-the-screen
+    //https://reactjs.org/docs/refs-and-the-dom.html
+    //https://stackoverflow.com/questions/55204205/a-way-to-count-columns-in-a-responsive-grid
+    //https://stackoverflow.com/questions/36209432/how-to-dynamically-add-a-class-to-manual-class-nameshttps://stackoverflow.com/questions/36209432/how-to-dynamically-add-a-class-to-manual-class-namesS
+    //https://stackoverflow.com/questions/6492683/how-to-detect-divs-dimension-changed
+    var node = this.ref.current;
+    if (node !== null) {
+      new ResizeObserver(() => {
+        if (node.parentNode === null) return;
+        var parentGridWidth = window
+          .getComputedStyle(node.parentNode)
+          .getPropertyValue('grid-template-columns')
+          .replace(' 0px', '')
+          .split(' ').length;
+
+        if (this.lastParentGridWidth == parentGridWidth) return;
+        this.lastParentGridWidth = parentGridWidth;
+
+        if (parentGridWidth == 1) this.setState({ dir: 1 });
+        else this.setState({ dir: this.props.dir });
+      }).observe(node.parentNode);
+    }
+
     if (this.state.info == this.props.info) return;
     this.setState({ info: this.props.info });
 
@@ -19,19 +50,16 @@ class Card extends Component {
   }
 
   render() {
-    this.direction = this.props.dir ? 'row' : 'col';
-    this.inverted = this.props.inv ? 'inv' : 'nor';
+    var direction = this.state.dir ? 'row' : 'col';
+    var inverted = this.props.inv ? 'inv' : 'nor';
     if (this.state.id === undefined) return <Loading height="100vh" />;
 
     var Card2 = React.forwardRef(({ onClick, href }, ref) => (
       <React.Fragment>
         <a
+          ref={this.ref}
           className={
-            styles.card +
-            ' ' +
-            styles[this.inverted] +
-            ' ' +
-            styles[this.direction]
+            styles.card + ' ' + styles[inverted] + ' ' + styles[direction]
           }
           href={href}
           onClick={onClick}
