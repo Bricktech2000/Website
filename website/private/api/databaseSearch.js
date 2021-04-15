84210226615bac78e973a4efcaace0330c0eb36e
Bricktech2000 <bricktech2000@gmail.com>
2021-04-15 12:23:19 -0400

    doubled performance of website with two `/`

--- website/private/api/databaseSearch.js
+++ website/private/api/databaseSearch.js
@@ -1,7 +1,7 @@
 export default async function getPostInfo(params) {
   //see getPostInfo
   return await (
-    await fetch('/api/databaseSearch', {
+    await fetch('/api/databaseSearch/', {
       method: 'POST',
       body: JSON.stringify({ params: params }),
     })
--- website/private/api/getPostInfo.js
+++ website/private/api/getPostInfo.js
@@ -1,7 +1,7 @@
 export default async function getPostInfo(ids) {
   //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   return await (
-    await fetch('/api/getPostInfo', {
+    await fetch('/api/getPostInfo/', {
       method: 'POST',
       body: JSON.stringify({ ids: ids }),
     })
