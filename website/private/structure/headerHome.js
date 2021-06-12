237e751d000f7b9c552c6856841c2b2c0e2ce9af
Bricktech2000 <bricktech2000@gmail.com>
2021-06-11 22:56:33 -0400

    created homepage about section

--- website/private/structure/headerHome.js
+++ website/private/structure/headerHome.js
@@ -10,7 +10,18 @@ class HeaderHome extends Component {
       <header className={styles.Header}>
         <div className={styles.color}></div>
         <div className={styles.image}></div>
-        <div className={styles.about}>about</div>
+        <div className={styles.about}>
+          <h1 className="markup-h1">A bit more about myself</h1>
+          <img src="/icon.png" />
+          <p>
+            Minim nulla id eiusmod ea quis exercitation in deserunt. Non
+            excepteur exercitation ullamco consectetur Lorem officia. Occaecat
+            est do et ex dolor consequat sit sunt laboris do aliquip nisi. Sit
+            adipisicing nostrud proident velit commodo proident qui cillum
+            aliquip qui sunt et incididunt.
+          </p>
+          <div className={styles.icons}>O O O O</div>
+        </div>
         <div className={styles.overlay}>
           <div className={styles.title}>Let me show you what I can do.</div>
           <div className={styles.desc}>
