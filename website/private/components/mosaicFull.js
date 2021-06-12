dffe10a9ddfa1b97dd9b7fa3433daa8a1da41e7a
Bricktech2000 <bricktech2000@gmail.com>
2021-06-12 11:49:20 -0400

    created mosaic parallax animation

--- website/private/components/mosaicFull.js
+++ website/private/components/mosaicFull.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import generator from '../api/rand';
+import parallax from '../api/parallax';
 
 import styles from './mosaicFull.module.css';
 
 class MosaicFull extends Component {
   state = {};
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.rand = generator();
+
+    [this.componentDidMount, this.componentDidUnmount, this.parallaxRef] =
+      parallax((current, value) => {
+        current.style.transform = `translateY(calc(var(--smart-unit) * ${
+          (value - 0.25) * -20
+        }))`;
+      });
   }
 
   render() {
     return (
-      <div className={styles.MosaicFull}>
+      <div className={styles.MosaicFull} ref={this.parallaxRef}>
         {this.props.children.map((child, i) =>
           React.cloneElement(child, {
             dir: this.rand() > 0.5,
