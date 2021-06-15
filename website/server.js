431a16cfefc2d92a611844b36fa0a4d471f8e47a
Bricktech2000 <bricktech2000@gmail.com>
2021-06-14 22:00:22 -0400

    fixed syntax error when using `https`

--- website/server.js
+++ website/server.js
@@ -2,7 +2,7 @@ const https = false;
 const dev = false;
 
 const { createServer } = require(https ? 'https' : 'http');
-if(https) const http = require('http');
+if (https) var http = require('http');
 const { parse } = require('url');
 const next = require('next');
 const fs = require('fs');
