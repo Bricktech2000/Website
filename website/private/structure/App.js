6843732bd7507fdea616221b29ec5a9e371b3f3e
Bricktech2000 <bricktech2000@gmail.com>
2021-07-20 13:43:00 -0400

    improved color theme change code

--- website/private/structure/App.js
+++ website/private/structure/App.js
@@ -18,28 +18,26 @@ class App extends Component {
     //https://stackoverflow.com/questions/49411796/how-do-i-detect-i-am-on-server-vs-client-in-next-js
     if (!process.browser) return;
 
-    var colors;
-    var defaults = ['#ff8f0d', '#df6f08', '#df4f00', '#fa0'];
-    var storage = JSON.parse(localStorage.getItem('lastColors'));
-    if (this.count === 0) colors = storage || defaults;
-    else if (this.count === 1 && storage.toString() !== defaults.toString())
-      colors = defaults;
-    else {
-      var color = Math.floor(Math.random() * 360);
-      colors = [
-        `hsl(${color}, 75%, 60%)`,
-        `hsl(${color}, 75%, 50%)`,
-        `hsl(${color}, 75%, 40%)`,
-        `hsl(${color + 30}, 75%, 50%)`,
-      ];
-    }
-    document.documentElement.style.setProperty('--color-l', colors[0]);
-    document.documentElement.style.setProperty('--color', colors[1]);
-    document.documentElement.style.setProperty('--color-d', colors[2]);
-    document.documentElement.style.setProperty('--color-h', colors[3]);
+    var colorHue;
+    var defaultHue = '25'; //orange
+    var storageHue = JSON.parse(localStorage.getItem('colorHue'));
+    if (this.count === 0) colorHue = storageHue || defaultHue;
+    else if (this.count === 1 && JSON.parse(storageHue) !== defaultHue)
+      colorHue = defaultHue;
+    else colorHue = Math.floor(Math.random() * 360);
+
+    var cssColors = {
+      '--color-l': `hsl(${colorHue}, 75%, 60%)`,
+      '--color': `hsl(${colorHue}, 75%, 50%)`,
+      '--color-d': `hsl(${colorHue}, 75%, 40%)`,
+      '--color-h': `hsl(${colorHue + 30}, 75%, 50%)`,
+    };
+    //}
+    for (var cssName in cssColors)
+      document.documentElement.style.setProperty(cssName, cssColors[cssName]);
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => (this.count = 1), 1000);
-    localStorage.setItem('lastColors', JSON.stringify(colors));
+    localStorage.setItem('lastColorHue', JSON.stringify(colorHue));
     this.count++;
   };
 
