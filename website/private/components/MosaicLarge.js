df8cf5cb2af65492adf972de6fdf4abe77792167
Bricktech2000 <bricktech2000@gmail.com>
2021-07-23 13:55:03 -0400

    created 'Load More' button for `MosaicLarge` component

--- website/private/components/MosaicLarge.js
+++ website/private/components/MosaicLarge.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import Button from './Button';
 import generator from '../lib/rand';
 import parallax from '../lib/parallax';
 
 import styles from './MosaicLarge.module.css';
 
 class MosaicLarge extends Component {
-  state = {};
+  state = { clickCount: 3 };
 
   constructor(props) {
     super(props);
-    this.rand = generator();
 
     [
       this.componentDidMount,
@@ -23,13 +23,27 @@ class MosaicLarge extends Component {
   }
 
   render() {
+    this.rand = generator();
+    console.log(this.state.clickCount);
     return (
-      <div className={styles.MosaicLarge} ref={this.parallaxRef}>
-        {this.props.children.map((child, i) =>
-          React.cloneElement(child, {
-            dir: this.rand() > 0.5,
-            inv: this.rand() > 0.5,
-          })
+      <div className={styles.container}>
+        <div className={styles.MosaicLarge} ref={this.parallaxRef}>
+          {this.props.children
+            .slice(0, Math.pow(2, this.state.clickCount))
+            .map((child, i) =>
+              React.cloneElement(child, {
+                dir: this.rand() > 0.5,
+                inv: this.rand() > 0.5,
+              })
+            )}
+        </div>
+        {Math.pow(2, this.state.clickCount) < this.props.children.length && (
+          <Button
+            label="Load More"
+            onClick={() =>
+              this.setState({ clickCount: this.state.clickCount + 1 })
+            }
+          />
         )}
       </div>
     );
