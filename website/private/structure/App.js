632a3b8267f6dfcfde4a23610f37b6abd488ca06
Bricktech <Bricktech2000@gmail.com>
2021-08-14 13:54:51 -0400

    improved `App` code

--- website/private/structure/App.js
+++ website/private/structure/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import Head from './Head';
 
 import { domain } from '../lib/consts';
@@ -6,28 +6,27 @@ import styles from './App.module.css';
 import { init } from '../lib/rand';
 init();
 
-class App extends Component {
-  state = {};
-  constructor() {
-    super();
-    this.count = 0;
-    this.changePalette({ preventDefault: () => {} });
-  }
+const App = (props) => {
+  const [count, setCount] = useState(0);
 
-  changePalette = (e) => {
+  useEffect(() => {
+    changePalette({ preventDefault: () => {} });
+  }, []);
+
+  const changePalette = (e) => {
     e.preventDefault();
     //https://stackoverflow.com/questions/49411796/how-do-i-detect-i-am-on-server-vs-client-in-next-js
     if (!process.browser) return;
 
     var colorHue;
-    var defaultHue = '216'; //greenish blue
-    var storageHue = localStorage.getItem('colorHue');
-    if (this.count === 0) colorHue = storageHue || defaultHue;
-    else if (this.count === 1 && storageHue !== defaultHue)
-      colorHue = defaultHue;
+    var timeout;
+    const defaultHue = '216'; //greenish blue
+    const storageHue = localStorage.getItem('colorHue');
+    if (count == 0) colorHue = storageHue || defaultHue;
+    else if (count == 1 && storageHue !== defaultHue) colorHue = defaultHue;
     else colorHue = '' + Math.floor(Math.random() * 360);
 
-    var cssColors = {
+    const cssColors = {
       '--color-l': `hsl(${colorHue}, 75%, 60%)`,
       '--color': `hsl(${colorHue}, 75%, 50%)`,
       '--color-d': `hsl(${colorHue}, 75%, 40%)`,
@@ -36,24 +35,24 @@ class App extends Component {
 
     for (var cssName in cssColors)
       document.documentElement.style.setProperty(cssName, cssColors[cssName]);
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => (this.count = 1), 1000);
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      setCount(1);
+    }, 1000);
     localStorage.setItem('lastColorHue', colorHue);
-    this.count++;
+    setCount((c) => c + 1);
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <Head
-          title={`Emilien Breton | ${this.props.title}`}
-          description={this.props.description}
-          image={`https://${domain}/${this.props.image}`}
-        />
-        <div className={styles.App}>{this.props.children}</div>
-      </React.Fragment>
-    );
-  }
-}
+  return (
+    <React.Fragment>
+      <Head
+        title={`Emilien Breton | ${props.title}`}
+        description={props.description}
+        image={`https://${domain}/${props.image}`}
+      />
+      <div className={styles.App}>{props.children}</div>
+    </React.Fragment>
+  );
+};
 
 export default App;
