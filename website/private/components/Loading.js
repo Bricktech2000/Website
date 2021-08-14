936b7c491877021f183655d3df2e3b6629ffc98d
Bricktech <Bricktech2000@gmail.com>
2021-08-14 01:23:42 -0400

    improved `Loading` code

--- website/private/components/Loading.js
+++ website/private/components/Loading.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 
-class Loading extends Component {
-  state = {};
-  render() {
-    return <div style={{ height: this.props.height || '50vh' }}></div>;
-  }
-}
+const Loading = (props) => {
+  return <div style={{ height: props.height || '50vh' }}></div>;
+};
 
 export default Loading;
