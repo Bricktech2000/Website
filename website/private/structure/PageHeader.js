879c27ab56c6cab966819d81051681c17bfb1c92
Bricktech <Bricktech2000@gmail.com>
2021-08-14 14:07:43 -0400

    improved `PageHeader` code

--- website/private/structure/PageHeader.js
+++ website/private/structure/PageHeader.js
@@ -4,23 +4,21 @@ import Marked from '../components/Marked';
 
 import styles from './PageHeader.module.css';
 
-class PageHeader extends Component {
-  render() {
-    return (
-      <header className={styles.PageHeader}>
-        <div className={styles['markup-h1'] + ' markup-h1' + ' fade-right-1'}>
-          {this.props.info.title}
-        </div>
-        <div></div>
-        <p className={'fade-right-2'}>
-          <Marked source={this.props.info.desc} />
-        </p>
-        <div className={styles.tags + ' fade-right-3'}>
-          <Date date={this.props.info.date} />
-        </div>
-      </header>
-    );
-  }
-}
+const PageHeader = (props) => {
+  return (
+    <header className={styles.PageHeader}>
+      <div className={styles['markup-h1'] + ' markup-h1' + ' fade-right-1'}>
+        {props.info.title}
+      </div>
+      <div></div>
+      <p className={'fade-right-2'}>
+        <Marked source={props.info.desc} />
+      </p>
+      <div className={styles.tags + ' fade-right-3'}>
+        <Date date={props.info.date} />
+      </div>
+    </header>
+  );
+};
 
 export default PageHeader;
