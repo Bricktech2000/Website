7634cf8219f479ebc574375e128549136d70ce81
Bricktech <Bricktech2000@gmail.com>
2021-08-14 01:18:08 -0400

    improved `Date` code

--- website/private/components/Date.js
+++ website/private/components/Date.js
@@ -3,11 +3,8 @@ import Tag from './Tag';
 
 import formatDate from '../lib/formatDate.js';
 
-class Date extends Component {
-  state = {};
-  render() {
-    return <Tag label={formatDate(this.props.date)} active />;
-  }
-}
+const Date = (props) => {
+  return <Tag label={formatDate(props.date)} active />;
+};
 
 export default Date;
