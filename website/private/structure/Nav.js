6ab6aae78a0972cb782e6860c03c2ac168062919
Bricktech <Bricktech2000@gmail.com>
2021-08-14 14:05:03 -0400

    improved `Nav` code

--- website/private/structure/Nav.js
+++ website/private/structure/Nav.js
@@ -4,8 +4,8 @@ import { useRouter } from 'next/router';
 import styles from './Nav.module.css';
 import BackArrow from '../svgs/012-arrow-2.svg';
 
-export default function Nav() {
-  var router = useRouter();
+const Nav = () => {
+  const router = useRouter();
 
   //https://stackoverflow.com/questions/57946028/issue-with-window-object-on-a-functional-react-component
   //https://nextjs.org/docs/api-reference/next/router
@@ -21,4 +21,6 @@ export default function Nav() {
       <BackArrow />
     </div>
   );
-}
+};
+
+export default Nav;
