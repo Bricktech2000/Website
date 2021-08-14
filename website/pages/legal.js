fa846a1cd226468769d844ce0aabcecba8f0323d
Bricktech <Bricktech2000@gmail.com>
2021-08-14 14:23:19 -0400

    improved `legal` code

--- website/pages/legal.js
+++ website/pages/legal.js
@@ -9,18 +9,18 @@ import PageHeader from '../private/structure/PageHeader';
 import Loading from '../private/components/Loading';
 import dbGet from '../private/lib/dbGet';
 
-export default function Legal(props) {
+const Legal = (props) => {
   const id = 'legal';
   //https://stackoverflow.com/questions/53819864/how-to-async-await-in-react-render-function
   const [info, updateInfo] = useState();
   useEffect(() => {
-    const getInfo = async () => {
+    (async () => {
       updateInfo(await dbGet('exact', id));
-    };
-    getInfo();
+    })();
   }, [props]);
 
-  var loading = typeof info === 'undefined' || typeof info[id] === 'undefined';
+  const loading =
+    typeof info === 'undefined' || typeof info[id] === 'undefined';
   return (
     <App title="Legal" description="" image="icon.png">
       <br />
@@ -40,4 +40,6 @@ export default function Legal(props) {
       )}
     </App>
   );
-}
+};
+
+export default Legal;
