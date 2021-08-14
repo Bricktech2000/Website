19ca3a15ac8448a3c5b5898963803b8f06a2ee04
Bricktech <Bricktech2000@gmail.com>
2021-08-14 01:46:59 -0400

    improved `MosaicLarge` code

--- website/private/components/MosaicLarge.js
+++ website/private/components/MosaicLarge.js
@@ -1,63 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Button from './Button';
 import generator from '../lib/rand';
 import parallax from '../lib/parallax';
 
 import styles from './MosaicLarge.module.css';
 
-class MosaicLarge extends Component {
-  state = {};
+const MosaicLarge = (props) => {
+  const parallaxRef = parallax((current, value) => {
+    current.style.transform = `translateY(calc(var(--smart-unit) * ${(value -
+      0.25) *
+      -20}))`;
+  });
 
-  constructor(props) {
-    super(props);
+  //this is the most hacky solution ever
+  //modifying a property on an import to keep track of a value... can't get much worse than that
+  generator.clickCount = generator.clickCount || 3;
+  const [clickCount, setClickCount] = useState(generator.clickCount);
 
-    [
-      this.componentDidMount2,
-      this.componentWillUnmount,
-      this.parallaxRef,
-    ] = parallax((current, value) => {
-      current.style.transform = `translateY(calc(var(--smart-unit) * ${(value -
-        0.25) *
-        -20}))`;
-    });
-  }
+  const rand = generator();
 
-  componentDidMount() {
-    //this is the most hacky solution ever
-    //modifying a property on an import to keep track of a value... can't get much worse than that
-
-    generator.clickCount = generator.clickCount || 3;
-    this.setState({ clickCount: generator.clickCount });
-    this.componentDidMount2();
-  }
-
-  render() {
-    this.rand = generator();
-    return (
-      <div className={styles.container}>
-        <div className={styles.MosaicLarge} ref={this.parallaxRef}>
-          {this.props.children
-            .slice(0, Math.pow(2, this.state.clickCount))
-            .map((child, i) =>
-              React.cloneElement(child, {
-                dir: this.rand() > 0.5,
-                inv: this.rand() > 0.5,
-              })
-            )}
-        </div>
-        {Math.pow(2, this.state.clickCount) < this.props.children.length && (
-          <Button
-            label="Load More"
-            onClick={() => {
-              this.setState({ clickCount: this.state.clickCount + 1 });
-              generator.clickCount++;
-              console.log(generator.clickCount);
-            }}
-          />
+  return (
+    <div className={styles.container}>
+      <div className={styles.MosaicLarge} ref={parallaxRef}>
+        {props.children.slice(0, Math.pow(2, clickCount)).map((child, i) =>
+          React.cloneElement(child, {
+            dir: rand() > 0.5,
+            inv: rand() > 0.5,
+          })
         )}
       </div>
-    );
-  }
-}
+      {Math.pow(2, clickCount) < props.children.length && (
+        <Button
+          label="Load More"
+          onClick={() => {
+            setClickCount((c) => c + 1);
+            generator.clickCount++;
+          }}
+        />
+      )}
+    </div>
+  );
+};
 
 export default MosaicLarge;
