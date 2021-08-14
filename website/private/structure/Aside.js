5b7ceb45d353a52985693c3de6a4273ffc8cbb67
Bricktech <Bricktech2000@gmail.com>
2021-08-14 13:55:14 -0400

    improved `Aside` code

--- website/private/structure/Aside.js
+++ website/private/structure/Aside.js
@@ -2,11 +2,8 @@ import React, { Component } from 'react';
 
 import styles from './Aside.module.css';
 
-class Aside extends Component {
-  state = {};
-  render() {
-    return <aside className={styles.Aside}></aside>;
-  }
-}
+const Aside = () => {
+  return <aside className={styles.Aside}></aside>;
+};
 
 export default Aside;
