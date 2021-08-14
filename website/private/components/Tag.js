46256115fdad6da9ae909f5436ba225e61a7d23d
Bricktech <Bricktech2000@gmail.com>
2021-08-14 13:04:43 -0400

    improved `Tag` code

--- website/private/components/Tag.js
+++ website/private/components/Tag.js
@@ -3,21 +3,20 @@ import tagMap from '../lib/tagMap';
 
 import styles from './Tag.module.css';
 
-class Tag extends Component {
-  state = {};
-  render() {
-    var tagClassType = styles[tagMap[this.props.label]];
-    var tagLabel = this.props.label;
-    var tagActive = this.props.active;
-
-    tagActive = tagActive ? 'on' : 'off';
-    var tagClassActive = styles[tagActive];
-    return (
-      <div className={styles.Tag + ' ' + tagClassType + ' ' + tagClassActive}>
-        {tagLabel}
-      </div>
-    );
-  }
-}
+const Tag = (props) => {
+  return (
+    <div
+      className={
+        styles.Tag +
+        ' ' +
+        styles[tagMap[props.label]] +
+        ' ' +
+        styles[props.active ? 'on' : 'off']
+      }
+    >
+      {props.label}
+    </div>
+  );
+};
 
 export default Tag;
