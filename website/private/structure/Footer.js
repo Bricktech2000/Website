7b2508f02fc0812274c66b817326c565a3584c04
Bricktech <Bricktech2000@gmail.com>
2021-08-14 00:55:22 -0400

    improved `Footer` code

--- website/private/structure/Footer.js
+++ website/private/structure/Footer.js
@@ -10,45 +10,40 @@ import Ubuntu from '../svgs/000-ubuntu.svg';
 import Cloudflare from '../svgs/000-cloudflare.svg';
 import styles from './Footer.module.css';
 
-class Footer extends Component {
-  state = {};
-  render() {
-    return (
-      <footer className={styles.Footer}>
-        <div className={styles.vr}></div>
-        <div className={styles.center}>
-          <div>
-            Created with <React /> <NextJS /> <NodeJS /> by Emilien Breton
-          </div>
-          <br />
-          <div>
-            Powered and protected by <Ubuntu /> <Cloudflare />
-          </div>
-          <br />
-          <div>
-            &copy; {new Date().getFullYear()} Emilien Breton
-          </div> &#8212;{' '}
-          <Link href="/legal/">
-            <a className="markup-link">Legal</a>
-          </Link>
+const Footer = (props) => {
+  return (
+    <footer className={styles.Footer}>
+      <div className={styles.vr}></div>
+      <div className={styles.center}>
+        <div>
+          Created with <React /> <NextJS /> <NodeJS /> by Emilien Breton
         </div>
-        <div className={styles.contact}>
-          <GetInTouch />
-          <div className={styles.issue}>
-            Find an issue with this page?{' '}
-            <a
-              href={github + '/tree/master/website/' + this.props.github}
-              target="_blank"
-              rel="noreferrer"
-              className="markup-link"
-            >
-              Fix it on Github
-            </a>
-          </div>
+        <br />
+        <div>
+          Powered and protected by <Ubuntu /> <Cloudflare />
         </div>
-      </footer>
-    );
-  }
-}
+        <br />
+        <div>&copy; {new Date().getFullYear()} Emilien Breton</div> &#8212;{' '}
+        <Link href="/legal/">
+          <a className="markup-link">Legal</a>
+        </Link>
+      </div>
+      <div className={styles.contact}>
+        <GetInTouch />
+        <div className={styles.issue}>
+          Find an issue with this page?{' '}
+          <a
+            href={github + '/tree/master/website/' + props.github}
+            target="_blank"
+            rel="noreferrer"
+            className="markup-link"
+          >
+            Fix it on Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
