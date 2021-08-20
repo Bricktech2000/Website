f4708c85e47d828c1ad1b453aeb60dc1b99413a4
Bricktech <Bricktech2000@gmail.com>
2021-08-19 23:06:02 -0400

    fixed browser not scrolling to hash on page load

--- website/private/structure/PostMain.js
+++ website/private/structure/PostMain.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import Marked from '../components/Marked';
 import useParallax from '../lib/useParallax';
 
@@ -11,6 +11,22 @@ const PostMain = (props) => {
       -20}))`;
     current.style.opacity = Math.max(value * 2 - 0.5, 0);
   });
+
+  //https://stackoverflow.com/questions/9757625/jquery-how-to-scroll-to-certain-anchor-div-on-page-load
+  //https://stackoverflow.com/questions/13905435/javascript-getting-specific-element-of-parent-by-name
+  useEffect(() => {
+    // const anchor = parallaxRef.current.querySelector(
+    //   `[name='${window.location.hash.substring(1)}']`
+    // );
+    // anchor && anchor.scrollIntoView(true);
+
+    //hacky, but seems to be the easiest way to scroll to location.hash
+    setTimeout(() => {
+      const anchor = document.createElement('a');
+      anchor.href = window.location.hash;
+      window.location.hash !== '' && anchor.click();
+    }, 0);
+  }, [props.info.source, window.location.hash]);
 
   return (
     <div ref={parallaxRef} className={styles['marked']}>
