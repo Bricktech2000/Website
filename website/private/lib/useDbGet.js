a7623835c4df0cdd88f3a7671516a6ea3a47ab86
Bricktech <Bricktech2000@gmail.com>
2021-08-24 18:43:32 -0400

    improved `useDbGet` hook

--- website/private/lib/useDbGet.js
+++ website/private/lib/useDbGet.js
@@ -5,10 +5,12 @@ const useDbGet = (type, id) => {
   //https://stackoverflow.com/questions/53819864/how-to-async-await-in-react-render-function
   const [info, updateInfo] = useState();
   useEffect(() => {
+    //https://youtu.be/f687hBjwFcM?t=1876
+    updateInfo((info) => info);
     (async () => {
       updateInfo(await dbGet(type, id));
     })();
-  }, [type, id]);
+  }, [id]);
 
   return info;
 };
