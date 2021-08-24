e1288a1f52fd0bded24dbebd2abf8a3eba4f56a7
Bricktech <Bricktech2000@gmail.com>
2021-08-24 15:06:27 -0400

    implemented `ParentHeader` structure

--- website/private/structure/PostHeader.js
+++ website/private/structure/PostHeader.js
@@ -9,29 +9,35 @@ import styles from './PostHeader.module.css';
 const PostHeader = (props) => {
   return (
     <header className={styles.PostHeader}>
-      <img
-        src={'/' + props.info.id + '/index.jpg'}
-        alt={props.info.title + ' thumbnail image'}
-      />
-      <div className={styles['markup-h1'] + ' markup-h1' + ' fade-right-1'}>
-        {props.info.title}
+      <div className={styles.PostParentHeader}>
+        <h1>Parent Header</h1>
+        aoeuaOEUaoeu
       </div>
-      <p className={'fade-right-2'}>
-        <Marked source={props.info.desc} />
-      </p>
-      <div className={styles.tags + ' fade-right-3'}>
-        <Date date={props.info.date} />
-        {props.info.tags.map((tag) => (
-          <Tag key={tag} label={tag} />
-        ))}
-      </div>
-      <div className={styles.buttons + ' fade-right-3'}>
-        {Object.keys(props.info.btns).map((key) => {
-          var href = props.info.btns[key];
-          const blank = !href.match(/^[\.#]/);
-          if (!blank) href = '/' + props.info.id + '/' + href;
-          return <Button key={key} label={key} blank={blank} href={href} />;
-        })}
+      <div className={styles.PostDataHeader}>
+        <img
+          src={'/' + props.info.id + '/index.jpg'}
+          alt={props.info.title + ' thumbnail image'}
+        />
+        <div className={styles['markup-h1'] + ' markup-h1' + ' fade-right-1'}>
+          {props.info.title}
+        </div>
+        <p className={'fade-right-2'}>
+          <Marked source={props.info.desc} />
+        </p>
+        <div className={styles.tags + ' fade-right-3'}>
+          <Date date={props.info.date} />
+          {props.info.tags.map((tag) => (
+            <Tag key={tag} label={tag} />
+          ))}
+        </div>
+        <div className={styles.buttons + ' fade-right-3'}>
+          {Object.keys(props.info.btns).map((key) => {
+            var href = props.info.btns[key];
+            const blank = !href.match(/^[\.#]/);
+            if (!blank) href = '/' + props.info.id + '/' + href;
+            return <Button key={key} label={key} blank={blank} href={href} />;
+          })}
+        </div>
       </div>
     </header>
   );
