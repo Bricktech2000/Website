98cb944ad760d8b7d095b54fca159e6e7c40d409
Bricktech <Bricktech2000@gmail.com>
2021-08-27 15:01:45 -0400

    fixed component `ErrorMain`

--- website/private/structure/ErrorMain.js
+++ website/private/structure/ErrorMain.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '../components/Button';
 
 import styles from './ErrorMain.module.css';
+import { Marked as marked } from '../components/Marked.module.css';
 
 //https://www.google.com/search?q=404+error+page&sxsrf=ALeKk02OhUGx_72KKVvWMhcLf36Nj0OOJw:1618849064091&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjOy6Pb2orwAhUKY8AKHcWTCI4Q_AUoAXoECAEQAw&biw=1872&bih=1007#imgrc=pEPgPbeNV1OloM
 const ErrorMain = (props) => {
@@ -10,7 +11,7 @@ const ErrorMain = (props) => {
       <div />
       <h1 className={styles.header + ' fade-right-3'}>{props.info.id}</h1>
       <div className={styles.content}>
-        <h1 className={'markup-h1' + ' fade-right-1'}>{props.info.title}</h1>
+        <h1 className={marked}>{props.info.title}</h1>
         <p className={'fade-right-2'}>{props.info.desc}</p>
         <div className={styles.buttons + ' fade-right-3'}>
           {Object.keys(props.info.btns).map((key) => {
