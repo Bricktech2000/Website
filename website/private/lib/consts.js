a913f1efba11f98143e588f49c45cfe1b622dd7f
Bricktech <Bricktech2000@gmail.com>
2021-09-08 21:13:04 -0400

    replaced temporary discord link with server link

--- website/private/lib/consts.js
+++ website/private/lib/consts.js
@@ -5,6 +5,6 @@ export const social = {
   snapchat: 'https://www.snapchat.com/add/emilien_breton?share_id=09KhRyzndOk',
   facebook: 'https://www.facebook.com/emilien.breton.71',
   github: 'https://github.com/Bricktech2000/',
+  discord: 'https://discord.gg/vU9DA6jUz8',
   mail: 'bricktech2000@gmail.com',
-  discord: '/discord.html',
 };
