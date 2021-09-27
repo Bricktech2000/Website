d19b1141eabc029cf11cff0ddd7b2642cfb22bd9
Bricktech <Bricktech2000@gmail.com>
2021-09-27 12:57:17 -0400

    fixed closing tag

--- website/private/structure/Head.js
+++ website/private/structure/Head.js
@@ -39,14 +39,14 @@ const Head = (props) => {
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet"
-      ></link>
+      />
       {/* https://fonts.google.com/specimen/Inconsolata?category=Monospace */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
-      ></link>
+      />
     </NextHead>
   );
 };
