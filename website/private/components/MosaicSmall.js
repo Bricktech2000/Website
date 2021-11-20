e2d8488ac3330c0961113151267944ac28a64b8c
Bricktech2000 <bricktech2000@gmail.com>
2021-11-19 23:01:00 -0500

    fixed `MosaicSmall` with new `Card` hover effect

--- website/private/components/MosaicSmall.js
+++ website/private/components/MosaicSmall.js
@@ -2,33 +2,17 @@ import React, { Component, useState, useContext } from 'react';
 import useParallax from '../lib/useParallax';
 
 import styles from './MosaicSmall.module.css';
-import RandContext from '../lib/RandContext';
 
 const MosaicSmall = (props) => {
-  const rand = useContext(RandContext)();
-
   const parallaxRef = useParallax((current, value) => {
-    current.style.transform = `translateY(calc(0.5em * ${(value - 0.25) *
-      -16}))`;
+    current.style.transform = `translateY(calc(0.5em * ${
+      (value - 0.25) * -16
+    }))`;
   });
-
-  const firstIsRow = rand() > 0.5;
-  const secondIsRow = !firstIsRow;
-
-  const directions = [firstIsRow, firstIsRow, secondIsRow, secondIsRow];
-  //https://stackoverflow.com/questions/32370994/how-to-pass-props-to-this-props-children
-  const func = (start, end) =>
-    props.children.slice(start, end).map((child, i) =>
-      React.cloneElement(child, {
-        dir: directions[start + i],
-        inv: rand() > 0.5,
-      })
-    );
 
   return (
     <div className={styles.MosaicSmall} ref={parallaxRef}>
-      <div>{func(0, 2)}</div>
-      <div>{func(2, 4)}</div>
+      {props.children.map((child, i) => React.cloneElement(child))}
     </div>
   );
 };
