d1ccfb9161a1c09fbe52932abdd32a16ba92377d
Bricktech <bricktech2000@gmail.com>
2022-01-24 17:29:58 -0500

    removed unused styles

--- website/private/structure/HomePinned.js
+++ website/private/structure/HomePinned.js
@@ -3,10 +3,8 @@ import useDbGet from '../lib/useDbGet';
 import MosaicSmall from '../components/MosaicSmall';
 import Card from '../components/Card';
 import Loading from '../components/Loading';
-import Marked from '../components/Marked.js';
 
 import { Marked as marked } from '../components/Marked.module.css';
-import styles from './HomePinned.module.css';
 
 const HomePinned = () => {
   const info = useDbGet('pinned');
@@ -26,10 +24,6 @@ const HomePinned = () => {
         {Object.keys(info)
           .slice(0, 4)
           .map((id) => (
-            // <div key={id} className={styles.Pair}>
-            // <Card key={'Card'} info={info[id]} />
-            // <Marked key={'Marked'} source={info[id].pinned} />
-            // </div>
             <Card key={id} info={info[id]} />
           ))}
       </MosaicSmall>
