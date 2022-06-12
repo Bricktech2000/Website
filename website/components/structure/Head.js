12ee119a528b32dd1ab327b37dd8bb929e2ebe97
Bricktech <bricktech2000@gmail.com>
2022-06-12 02:24:16 -0400

    modified `Head` component

--- website/components/structure/Head.js
+++ website/components/structure/Head.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import NextHead from 'next/head';
-import Script from 'next/script';
 
 const Head = (props) => {
   return (
@@ -16,27 +15,18 @@ const Head = (props) => {
       <title>{props.title}</title>
       <meta name="description" content={props.description} />
       <meta name="apple-mobile-web-app-title" content="Emilien Breton" />
-      {/* https://stackoverflow.com/questions/2208933/how-do-i-force-a-favicon-refresh */}
       <link rel="shortcut icon" href="/icon.png?v=4" />
       <link rel="apple-touch-icon" href="/icon.png?v=4" />
-      {/* https://support.google.com/tagassistant/answer/2947093?hl=en */}
-      {/* https://chrome.google.com/webstore/detail/tag-assistant-by-google/kejbdjndbnbjgmefkgdddjlbokphdefk/related?hl=en */}
-      {/* https://developers.google.com/analytics/devguides/collection/gtagjs */}
-      {/* Global site tag (gtag.js) - Google Analytics */}
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=UA-163126541-1"
-        strategy="afterInteractive"
-      />
-      <script>
-        {/*window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-163126541-1');*/}
-      </script>
-      {/* End Global site tag */}
-      <script>{/*console.log('Google Test 0.16');*/}</script>
+
       {/* https://github.com/markedjs/marked */}
       {/* https://highlightjs.org/download/ */}
+
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
       {/* https://fonts.google.com/specimen/Roboto?category=Sans+Serif */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet"
