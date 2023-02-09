b5241474f42a744663632d55be9ea31ec08317af
Emilien Breton <bricktech2000@gmail.com>
2023-02-09 17:37:29 -0500

    improved dynamic accent color switching code quality

--- website/components/structure/App.js
+++ website/components/structure/App.js
@@ -13,37 +13,34 @@ const App = (props) => {
 
   const changePalette = (e) => {
     e.preventDefault();
-    // https://stackoverflow.com/questions/49411796/how-do-i-detect-i-am-on-server-vs-client-in-next-js
-    if (!process.browser) return;
 
     // reset hues that were previously used as the default hue in older versions of the site
     const oldHues = ['216', '203'];
     if (oldHues.includes(localStorage.getItem('colorHue')))
       localStorage.removeItem('colorHue');
 
+    const defaultHue = 210; // greenish blue
+    const storageHue = parseInt(localStorage.getItem('colorHue'));
+
     let colorHue;
-    let timeout;
-    const defaultHue = '210'; // greenish blue
-    const storageHue = localStorage.getItem('colorHue');
-    if (count == 0) colorHue = storageHue ?? defaultHue;
+    if (count == 0) colorHue = storageHue || defaultHue;
     else if (count == 1 && storageHue !== defaultHue) colorHue = defaultHue;
-    else colorHue = '' + Math.floor(Math.random() * 360);
+    else colorHue = Math.floor(Math.random() * 360);
 
     const cssColors = {
       '--color-l': `hsl(${colorHue}, 100%, 67%)`,
       '--color': `hsl(${colorHue}, 100%, 57%)`,
       '--color-d': `hsl(${colorHue}, 100%, 47%)`,
-      '--color-h': `hsl(${parseInt(colorHue) + 15}, 75%, 50%)`,
     };
 
     for (let cssName in cssColors)
       document.documentElement.style.setProperty(cssName, cssColors[cssName]);
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      setCount(1);
-    }, 1000);
+
     localStorage.setItem('colorHue', colorHue);
     setCount((c) => c + 1);
+    setTimeout(() => {
+      setCount(1);
+    }, 1000);
   };
 
   return (
