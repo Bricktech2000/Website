789e5d3c69f282d788721cc49bf0bb154e20a723
Emilien Breton <bricktech2000@gmail.com>
2023-03-20 14:44:31 -0400

    updated path to resume

--- website/records/consts.js
+++ website/records/consts.js
@@ -2,7 +2,7 @@ export const language = 'en';
 export const domain = 'emilien.ca';
 export const github = 'https://github.com/Bricktech2000/Website';
 export const resume =
-  'https://github.com/Bricktech2000/Resume/raw/master/export/resume.pdf';
+  'https://github.com/Bricktech2000/Resume/raw/master/export/resume.dark.pdf';
 export const social = {
   github: 'https://github.com/Bricktech2000/',
   discord: 'https://discord.gg/vU9DA6jUz8',
